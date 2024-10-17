812491529d062763973a6bd1e0f0c146fb3d4961
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-10-17 20:29:59 +0530

    Authentication

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const __dirname = path.resolve();
 
 // setting up cors 
 app.use(cors({
-    origin: "http://localhost:3000", credentials: true
+    origin: "http://localhost:5173", credentials: true
 }));
 
 app.use(express.json());        // allows us to parse incoming requests: req.body
@@ -26,11 +26,11 @@ app.use('/api/auth', authRoutes)
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    })
+    });
 }
 app.listen(PORT, () => {
     connectDB();
